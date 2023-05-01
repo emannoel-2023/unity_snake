cac04b814ce796683ca7edbb0efc04a1363202aa
emannoel-2023 <emannoel.2023@gmail.com>
2023-05-01 17:07:08 -0300

    update headMoves

--- Assets/Scripts/HeadMove.cs
+++ Assets/Scripts/HeadMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class HeadMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector2 direction;
+
+    private void Update()
     {
-        
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float yAxis = Input.GetAxisRaw("Vertical");
+
+        if(xAxis != 0)
+        {
+            direction = vector2.right * xAxis;
+        }
+        if (yAxis != 0)
+        {
+            direction = vector2.up * yAxis;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void MoveSnake()
     {
-        
+
     }
 }
