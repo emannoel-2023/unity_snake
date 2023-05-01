3a151dd447ade864c3bc1baabcec73db782074fb
emannoel-2023 <emannoel.2023@gmail.com>
2023-05-01 20:41:41 -0300

    add score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text ScoreText;
+    [SerializeField] int score;
+}
